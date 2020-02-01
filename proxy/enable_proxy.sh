e63178c501fac89099b4574c10e93e2e201dcfa5
yuetsin <akaza_akari@sjtu.edu.cn>
2020-02-01 12:35:06 +0800

    [fix] port 1087

--- proxy/enable_proxy.sh
+++ proxy/enable_proxy.sh
@@ -3,5 +3,5 @@
 export http_proxy=http://127.0.0.1:1087
 export https_proxy=http://127.0.0.1:1087
 
-git config --global http.proxy 127.0.0.1:8087
+git config --global http.proxy 127.0.0.1:1087
 # git config --global https.proxy 127.0.0.1:1087
