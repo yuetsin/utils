204da70db896e46a4f9029b42f3b44433588c425
yuetsin <akaza_akari@sjtu.edu.cn>
2020-10-07 19:11:02 +0800

    [fix] automatically fill in answers

--- notes/python-challenges/fill_answers.py
+++ notes/python-challenges/fill_answers.py
@@ -5,17 +5,21 @@ from pwn import *
 
 r = remote('111.186.57.85', 30666)
 
-for i in range(1, 51):
-    if not i in results:
-        break
 
-    # r.recvuntil("Level:", drop=False)
-    r.recvrepeat(timeout=0.5)
-    r.sendline("%d" % i)
-    print("select problem %d" % i)
+try:
+    for i in range(1, 51):
+        if not i in results:
+            break
 
-    r.recvrepeat(timeout=0.2)
-    r.sendline(repr(results[i]))
-    print("answer problem %d with %s" % (i, repr(results[i])))
+        # r.recvuntil("Level:", drop=False)
+        r.recvrepeat(timeout=0.4)
+        r.sendline("%d" % i)
+        print("select problem %d" % i)
+
+        r.recvrepeat(timeout=0.1)
+        r.sendline(str(results[i]).encode('utf-8'))
+        print("answer problem %d with %s" % (i, repr(results[i])))
+except KeyboardInterrupt:
+    pass
 
 r.interactive()
